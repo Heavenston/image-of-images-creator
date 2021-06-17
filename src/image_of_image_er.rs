b0f9213dfb6850148a511c771abb056310bbad28
Heavenstone <malolegendrelemaire@gmail.com>
2021-06-17 18:09:58 +0200

    removed unused imports

--- src/image_of_image_er.rs
+++ src/image_of_image_er.rs
@@ -1,13 +1,12 @@
 use crate::image_dictionary::{ImageDictionary, DictionaryColor};
-use image::{DynamicImage, GenericImageView, Pixel, GenericImage, ImageBuffer, RgbImage, Rgb};
-use palette::{Srgb, Hsl};
+use image::{ RgbImage};
+use palette::{Srgb};
 
 pub fn image_of_image(dictionary: &ImageDictionary, target_image: &RgbImage) -> RgbImage {
     let mut new_image: RgbImage = RgbImage::new(target_image.width() * dictionary.images_size.0, target_image.height() * dictionary.images_size.1);
 
     target_image.enumerate_pixels()
         .for_each(|(x, y, p)| {
-            let p = target_image.get_pixel(x, y);
             let (_, image) = dictionary.get_closest(&DictionaryColor::from(Srgb::new(
                 p.0[0] as f32 / 255.,
                 p.0[1] as f32 / 255.,
