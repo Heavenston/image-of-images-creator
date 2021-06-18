7143bf3aa66f42a2646f9995ee0924da1ec186c2
Heavenstone <malolegendrelemaire@gmail.com>
2021-06-18 16:34:54 +0200

    Parallel image processing

--- library/src/image_of_image_er.rs
+++ library/src/image_of_image_er.rs
@@ -1,12 +1,16 @@
 use crate::image_dictionary::{ImageDictionary, DictionaryColor};
 use image::{ RgbImage};
 use palette::{Srgb};
+use rayon::prelude::*;
 
 pub fn image_of_image(dictionary: &ImageDictionary, target_image: &RgbImage) -> RgbImage {
     let mut new_image: RgbImage = RgbImage::new(target_image.width() * dictionary.images_size.0, target_image.height() * dictionary.images_size.1);
+    let image_ptr = &mut new_image as *mut RgbImage as usize;
 
     target_image.enumerate_pixels()
+        .par_bridge()
         .for_each(|(x, y, p)| {
+            let new_image = unsafe { &mut *(image_ptr as *mut RgbImage) };
             let (_, image) = dictionary.get_closest(&DictionaryColor::from(Srgb::new(
                 p.0[0] as f32 / 255.,
                 p.0[1] as f32 / 255.,
